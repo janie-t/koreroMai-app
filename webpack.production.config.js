1b5bf751b41f2cd9d88abd7867fb0839984c5f32
Janie Tito <jtito@hotmail.com>
2017-02-28 08:38:53 +1300

    explicitly using css-loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
-      { test: /\.scss$/, loaders: ['style','css','sass'] }
+      { test: /\.scss$/, loaders: ['style-loader','css-loader','sass-loader'] }
     ]
   },
 
